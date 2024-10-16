1325035961f18267a5f24e6a3c872b301a50db84
Flavio Bizzarri <33379291+newfla@users.noreply.github.com>
2024-10-16 16:16:42 +0200

    feat: diffusion.cpp bindings (#1)
    * feat: binding crate
    
    * cicd: fix ubuntu pipeline
    
    * ci: fix ubuntu runner v2
    
    * ci: fix ubuntu runner v3
    
    * ci: fix submodules pull
    
    * ci: try cuda on win2022 runner
    
    * ci: vulkan builder
    
    * fix(ci): install vulkan on all platforms
    
    * fix(ci): macos vulkan runner
    
    * feat: sycl support
    
    * fix(ci): sycl runner fix
    
    * ci: rocm runner introduction
    
    * fix(ci): cleanup space rocm runner
    
    * fix(ci): reworked build phase rocm runner
    
    * feat: windows hipblas support
    
    * ci: pipeline refactoring
    
    * doc: updated readme
    
    * fix: fmt

--- README.md
+++ README.md
@@ -1,2 +1,19 @@
 # diffusion-rs
 Rust bindings to https://github.com/leejet/stable-diffusion.cpp
+
+## Features Matrix
+| | Windows | Mac | Linux |
+| --- | :---: | :---: | :---: |
+|vulkan| ✅️ | ✅️ | ✅️ |
+|metal| ❌️ | ✅️ | ❌️ |
+|cuda| ✅️ | ❌️ | ✅️ |
+|rocm| ❔️ | ❌️ | ✅️ |
+|sycl| ❔️ | ❌️ | ✅️ |
+
+❔️: Not tested, should be supported 
+
+## Roadmap
+1. ~~Ensure that the underline cpp library compiles on supported platforms~~
+2. Build an easy to use library with models download and async interface
+3. Automatic library publishing on crates.io by gh actions
+4. _Maybe_ prebuilt CLI app binaries
---
+++ sys/build.rs
@@ -0,0 +1,195 @@
+use std::{
+    env,
+    fs::{create_dir_all, read_dir},
+    path::PathBuf,
+};
+
+use cmake::Config;
+use fs_extra::dir;
+
+// Heavily ispired by https://github.com/tazz4843/whisper-rs/blob/master/sys/build.rs
+
+fn main() {
+    // Link C++ standard library
+    let target = env::var("TARGET").unwrap();
+    if let Some(cpp_stdlib) = get_cpp_link_stdlib(&target) {
+        println!("cargo:rustc-link-lib=dylib={}", cpp_stdlib);
+    }
+
+    println!("cargo:rerun-if-changed=wrapper.h");
+
+    // Copy stable-diffusion code into the build script directory
+    let out = PathBuf::from(env::var("OUT_DIR").unwrap());
+    let diffusion_root = out.join("stable-diffusion.cpp/");
+
+    if !diffusion_root.exists() {
+        create_dir_all(&diffusion_root).unwrap();
+        dir::copy("./stable-diffusion.cpp", &out, &Default::default()).unwrap_or_else(|e| {
+            panic!(
+                "Failed to copy stable-diffusion sources into {}: {}",
+                diffusion_root.display(),
+                e
+            )
+        });
+    }
+
+    // Bindgen
+    let bindings = bindgen::Builder::default().header("wrapper.h");
+
+    bindings
+        .clang_arg("-I./stable-diffusion.cpp")
+        .clang_arg("-I./stable-diffusion.cpp/ggml/include")
+        .parse_callbacks(Box::new(bindgen::CargoCallbacks::new()))
+        .generate()
+        .unwrap()
+        .write_to_file(out.join("bindings.rs"))
+        .expect("Couldn't write bindings!");
+
+    // stop if we're on docs.rs
+    if env::var("DOCS_RS").is_ok() {
+        return;
+    }
+
+    // Configure cmake for building
+    let mut config = Config::new(&diffusion_root);
+
+    //Enable cmake feature flags
+    #[cfg(feature = "cublas")]
+    {
+        println!("cargo:rerun-if-env-changed=CUDA_PATH");
+        println!("cargo:rustc-link-lib=cublas");
+        println!("cargo:rustc-link-lib=cudart");
+        println!("cargo:rustc-link-lib=cublasLt");
+        println!("cargo:rustc-link-lib=cuda");
+
+        if target.contains("msvc") {
+            let cuda_path = PathBuf::from(env::var("CUDA_PATH").unwrap()).join("lib/x64");
+            println!("cargo:rustc-link-search={}", cuda_path.display());
+        } else {
+            println!("cargo:rustc-link-lib=culibos");
+            println!("cargo:rustc-link-search=/usr/local/cuda/lib64");
+            println!("cargo:rustc-link-search=/usr/local/cuda/lib64/stubs");
+            println!("cargo:rustc-link-search=/opt/cuda/lib64");
+            println!("cargo:rustc-link-search=/opt/cuda/lib64/stubs");
+        }
+
+        config.define("SD_CUBLAS", "ON");
+        if let Ok(target) = env::var("CUDA_COMPUTE_CAP") {
+            config.define("CUDA_COMPUTE_CAP", target);
+        }
+    }
+
+    #[cfg(feature = "hipblas")]
+    {
+        println!("cargo:rerun-if-env-changed=HIP_PATH");
+        println!("cargo:rustc-link-lib=hipblas");
+        println!("cargo:rustc-link-lib=rocblas");
+        println!("cargo:rustc-link-lib=amdhip64");
+
+        config.generator("Ninja");
+        config.define("CMAKE_C_COMPILER", "clang");
+        config.define("CMAKE_CXX_COMPILER", "clang++");
+        let hip_lib_path = if target.contains("msvc") {
+            let hip_path = env::var("HIP_PATH").expect("Missing HIP_PATH env variable");
+            PathBuf::from(hip_path).join("lib")
+        } else {
+            let hip_path = match env::var("HIP_PATH") {
+                Ok(path) => PathBuf::from(path),
+                Err(_) => PathBuf::from("/opt/rocm"),
+            };
+            hip_path.join("lib")
+        };
+        println!("cargo:rustc-link-search={}", hip_lib_path.display());
+        config.define("SD_HIPBLAS", "ON");
+        if let Ok(target) = env::var("AMDGPU_TARGETS") {
+            config.define("AMDGPU_TARGETS", target);
+        }
+    }
+
+    #[cfg(feature = "metal")]
+    {
+        println!("cargo:rustc-link-lib=framework=Foundation");
+        println!("cargo:rustc-link-lib=framework=Metal");
+        println!("cargo:rustc-link-lib=framework=MetalKit");
+        config.define("SD_METAL", "ON");
+    }
+
+    #[cfg(feature = "vulkan")]
+    {
+        if target.contains("msvc") {
+            println!("cargo:rerun-if-env-changed=VULKAN_SDK");
+            println!("cargo:rustc-link-lib=vulkan-1");
+            let vulkan_path = match env::var("VULKAN_SDK") {
+                Ok(path) => PathBuf::from(path),
+                Err(_) => panic!(
+                    "Please install Vulkan SDK and ensure that VULKAN_SDK env variable is set"
+                ),
+            };
+            let vulkan_lib_path = vulkan_path.join("Lib");
+            println!("cargo:rustc-link-search={}", vulkan_lib_path.display());
+        } else {
+            println!("cargo:rustc-link-lib=vulkan");
+        }
+        config.define("SD_VULKAN", "ON");
+    }
+
+    #[cfg(feature = "sycl")]
+    {
+        env::var("ONEAPI_ROOT").expect("Please load the oneAPi environment before building. See https://github.com/ggerganov/llama.cpp/blob/master/docs/backend/SYCL.md");
+
+        if target.contains("msvc") {
+            config.generator("Ninja");
+            config.define("CMAKE_C_COMPILER", "cl");
+            config.define("CMAKE_CXX_COMPILER", "icx");
+        } else {
+            config.define("CMAKE_C_COMPILER", "icx");
+            config.define("CMAKE_CXX_COMPILER", "icpx");
+        }
+        config.define("SD_SYCL", "ON");
+    }
+
+    #[cfg(feature = "flashattn")]
+    {
+        config.define("SD_FLASH_ATTN", "ON");
+        panic!("Broken in 2024/09/02 release!");
+    }
+
+    config
+        .profile("Release")
+        .define("SD_BUILD_SHARED_LIBS", "OFF")
+        .define("SD_BUILD_EXAMPLES", "OFF")
+        .very_verbose(true)
+        .pic(true);
+
+    let destination = config.build();
+
+    add_link_search_path(&out.join("lib")).unwrap();
+    add_link_search_path(&out.join("build")).unwrap();
+
+    println!("cargo:rustc-link-search=native={}", destination.display());
+    println!("cargo:rustc-link-lib=static=stable-diffusion");
+    println!("cargo:rustc-link-lib=static=ggml");
+}
+
+fn add_link_search_path(dir: &std::path::Path) -> std::io::Result<()> {
+    if dir.is_dir() {
+        println!("cargo:rustc-link-search={}", dir.display());
+        for entry in read_dir(dir)? {
+            add_link_search_path(&entry?.path())?;
+        }
+    }
+    Ok(())
+}
+
+// From https://github.com/alexcrichton/cc-rs/blob/fba7feded71ee4f63cfe885673ead6d7b4f2f454/src/lib.rs#L2462
+fn get_cpp_link_stdlib(target: &str) -> Option<&'static str> {
+    if target.contains("msvc") {
+        None
+    } else if target.contains("apple") || target.contains("freebsd") || target.contains("openbsd") {
+        Some("c++")
+    } else if target.contains("android") {
+        Some("c++_shared")
+    } else {
+        Some("stdc++")
+    }
+}
